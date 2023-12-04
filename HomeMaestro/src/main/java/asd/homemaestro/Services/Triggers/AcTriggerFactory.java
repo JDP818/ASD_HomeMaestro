d99349d65d2bd3110f541393dfe437f702633ac3
JDP818 <up202302818@up.pt>
2023-12-04 21:58:38 +0000

    Updated AcTriggerFileName

--- HomeMaestro/src/main/java/asd/homemaestro/Services/Triggers/AcTriggerFactory.java
+++ HomeMaestro/src/main/java/asd/homemaestro/Services/Triggers/AcTriggerFactory.java
@@ -1,14 +1,14 @@
 package asd.homemaestro.Services.Triggers;
 
-import asd.homemaestro.Entities.Triggers.AcTrigger;
+import asd.homemaestro.Entities.Triggers.AcTrigger2;
 import com.google.gson.Gson;
 import org.json.JSONObject;
 
 public class AcTriggerFactory {
-    public AcTrigger CreateAcTrigger(JSONObject jsonObject){
-        AcTrigger acTrigger = null;
+    public AcTrigger2 CreateAcTrigger(JSONObject jsonObject){
+        AcTrigger2 acTrigger = null;
         Gson gson = new Gson();
-        acTrigger = gson.fromJson(jsonObject.toString(), AcTrigger.class);
+        acTrigger = gson.fromJson(jsonObject.toString(), AcTrigger2.class);
         return  acTrigger;
     }
 }
--- HomeMaestro/src/main/java/asd/homemaestro/Services/Triggers/TriggerFactory.java
+++ HomeMaestro/src/main/java/asd/homemaestro/Services/Triggers/TriggerFactory.java
@@ -1,14 +1,10 @@
 package asd.homemaestro.Services.Triggers;
 
 import asd.Utils.Consts;
-import asd.homemaestro.Entities.Devices.DeviceCollection;
-import asd.homemaestro.Entities.Rooms.Room;
-import asd.homemaestro.Entities.Triggers.AcTrigger;
+import asd.homemaestro.Entities.Triggers.AcTrigger2;
 import asd.homemaestro.Entities.Triggers.Trigger;
 import asd.homemaestro.Entities.Triggers.TriggerType;
-import com.google.gson.Gson;
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +18,7 @@ public class TriggerFactory implements ITriggerFactory {
 
     @Override
     public Trigger createTrigger(String sensorId, String actuatorId, String sensorState, String actuatorState, TriggerType triggerType) {
-        return new AcTrigger(sensorId, actuatorId, sensorState, actuatorState, triggerType);
+        return new AcTrigger2(sensorId, actuatorId, sensorState, actuatorState, triggerType);
     }
 
     @Override
@@ -31,7 +27,7 @@ public class TriggerFactory implements ITriggerFactory {
         for(int i = 0; i < jsonArray.length(); i++){
             var jsonObject = jsonArray.getJSONObject(i);
             Trigger trigger = null;
-            if(jsonObject.get(Consts.JSONTYPE).toString().equalsIgnoreCase(AcTrigger.class.getSimpleName())
+            if(jsonObject.get(Consts.JSONTYPE).toString().equalsIgnoreCase(AcTrigger2.class.getSimpleName())
                 && jsonObject.get(Consts.JSONSENSORID).toString().equalsIgnoreCase(deviceId)){
                 trigger = acTriggerFactory.CreateAcTrigger(jsonObject);
             }
